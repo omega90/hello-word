2d04cf5b511655242c782422ed2a53f85f37f058
omega90 <ninasen223@gmail.com>
2016-04-14 22:42:31 +0200

    Added files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Daten aus einer Datei auf mehreren Seiten aufteilen</title>
+
+  <style>
+  body {
+   font-family: Verdana, Sans-Serif;
+   background-color: Oldlace;
+  }
+  a:link, a:visited {
+   text-decoration: None;
+   color: #FF0000;
+  }
+  </style>
+
+ </head>
+<body>
+<?php echo 'das ist ein tes '.' <br><strong> yes you can </strong>';
+
+
+?>
+
+<br>
+<h1>dfoijdfoijf</h1>
+
+</body>
+</html>
